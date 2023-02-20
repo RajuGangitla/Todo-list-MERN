a2d8fb30ba101deb49b97a3f6891907815ed1769
RajuGangitla <gangitlaraju8520@gmail.com>
2023-02-20 23:00:22 +0530

    changes done

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDb from "./config/db.js";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+//routes
 import authRouter from "./routes/userRoutes.js";
 import tasksRouter from "./routes/taskRoutes.js";
 import authenticateUser from "./middleware/authMiddleware.js";
 
 const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 //db connection
 mongoose.set("strictQuery", true);
@@ -17,6 +26,10 @@ connectDb();
 app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/tasks", authenticateUser, tasksRouter);
+
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  });
 
 const port = process.env.PORT || 8000;
 
